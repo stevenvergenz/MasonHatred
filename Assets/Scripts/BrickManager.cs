d1ef5aae6ef1fd0223584646136086c54e251bf5
Steven Vergenz <vergenzs@gmail.com>
2013-02-12 12:13:34 -0500

    Cosmetic changes
    Adjusted mouse sensitivity
    Updated ball prefab to mirror ball
    Changed brick layer pattern

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -6,16 +6,22 @@ public class BrickManager : MonoBehaviour
 	public Transform[] bricks;
 	
 	// brick spawn range: x[-8,8], y[0,4.75]
+	//private static float columns[] = {0.0f, 1.05f, 2.1f, 3.15f, 4.2f, 5.25f, 6.3f};
+	
 	// Use this for initialization
 	void Start ()
 	{
-		for( float i=-7; i<7; i+=1.05f )
+		int column = 0, row = 0;
+		for( float i=-6.3f; i<7; i+=1.05f )
 		{
+			row = 0;
 			for( float j=0; j<4.5f; j+=0.3f )
 			{
-				int type = Random.Range(0, bricks.Length);
+				int type = (row+column)%2;
 				Instantiate(bricks[type], new Vector3(i,j,0), Quaternion.identity);
+				row++;
 			}
+			column++;
 		}
 	}
 	
